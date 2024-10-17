aa39a7ca8f50e055c34add1a6858155a51da0405
AayushAnand39 <aayushanand737@gmail.com>
2024-10-17 18:15:59 +0530

    Third Commit

---
+++ postfix-evaluator/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar(){
+    const style = {
+        backgroundColor: "red", 
+        display:"flex",
+        color: "white",
+    };
+
+    return (
+        <div className="navbar" style={style}>
+            <h1>hello</h1>
+        </div>
+    );
+}
