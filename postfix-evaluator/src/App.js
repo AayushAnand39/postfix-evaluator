f16f83ca5f0dac226ca6ca35b82fd4493a1bfa00
AayushAnand39 <aayushanand737@gmail.com>
2024-10-31 00:36:19 +0530

    4

--- postfix-evaluator/src/App.js
+++ postfix-evaluator/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import Navbar from './components/Navbar';
+import Converter from './components/converter';
 import './App.css';
 
 function App() {
   return (
-    <>
+    <div>
       <Navbar/>
-    </>
+      <Converter/>
+    </div>
   );
 }
 
--- postfix-evaluator/src/components/Navbar.js
+++ postfix-evaluator/src/components/Navbar.js
@@ -1,15 +1,12 @@
 import React from 'react'
 
 export default function Navbar(){
-    const style = {
-        backgroundColor: "red", 
-        display:"flex",
-        color: "white",
-    };
-
     return (
-        <div className="navbar" style={style}>
-            <h1>hello</h1>
+        <div className="navbar" style={{backgroundColor: "orange", display: "flex", padding: "10px", height: "50px", fontFamily: "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif", fontWeight: "bolder", alignItems: "center", justifyContent: "space-between"}}>
+            <h2>The Postfix Checker</h2>
+            <button style={{color: "white", backgroundColor: "black", border: "5px solid purple", borderRadius: "10px"}}><h4>Enable Dark Mode</h4></button>
         </div>
+
+        
     );
 }
